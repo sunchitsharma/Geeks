69f64fc8d6e15672fabf9e5981ef76df675cdca5
sunchitsharma <sunchitsharma8@gmail.com>
2018-09-21 02:48:54 +0530

    2

---
+++ missing_number_in_array.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int sum=0;
+	    for(int i=0;i<n-1;++i)
+	    {
+	        int temp;
+	        cin>>temp;
+	        sum+=temp;
+	    }
+	    cout<<(n*(n+1))/2-sum<<endl;
+	}
+	return 0;
+}
